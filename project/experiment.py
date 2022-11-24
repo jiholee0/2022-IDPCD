68c564f504d6c635665bf9cfe487a3432a7f8f1a
이지호 <dlwlgh1254@naver.com>
2022-11-24 13:03:39 +0900

    gather dataset and experiment

--- project/experiment.py
+++ project/experiment.py
@@ -3,20 +3,23 @@ sys.path.append("/recog_gesture")
 import recog_gesture as recog_gesture
 import time
 
-kiosk_gesture = ['ZERO', 'ONE', 'TWO', 'FIVE', 'OK']
+kiosk_gesture = ['ZERO', 'ONE', 'TWO', 'FIVE', 'OK', 'GOOD']
 
-def compare(count) -> str :
+def compare(gesture, count) -> str :
     isCorrect = 0
+    isIncorrect = 0
     isFail = 0
     for i in range(0,count) :
-        for gesture in kiosk_gesture :
-            result = recog_gesture.recog_gesture()
-            print('return value : ', result , ' ', time.time(), '\n--------------\n')
-            if result == gesture : isCorrect += 1
-            if result == 'fail' : isFail += 1
-    return '인식률 ' + str(isCorrect / (count*5) * 100) + '% | 실패율 ' + str(isFail / (count*5) * 100) + '%'
+        result = recog_gesture.recog_gesture()
+        print(i+1, '. return value : ', result , time.time())
+        if result == gesture : isCorrect += 1
+        elif result == 'fail' : isFail += 1
+        else : isIncorrect += 1
+    return '인식률 ' + str(isCorrect / (count) * 100) + '% | 불일치율 ' + str(isIncorrect / count * 100) + '% | 실패율 ' + str(isFail / (count) * 100) + '%'
 
 # 인식률 계산
-count = 3
-compare(count)
+count = 5
+for gesture in kiosk_gesture :
+    print(gesture, ':', compare(gesture, count))
+    break
 
